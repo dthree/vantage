0a1ee7d0ea9c447e9bd7c9771260b3088a9ab7bd
dthree <threedeecee@gmail.com>
2015-08-03 10:49:25 -0700

    hapi support

---
+++ examples/hapi/README.md
@@ -0,0 +1,12 @@
+# Vantage Server with Hapi.js
+
+This is an example of a Vantage server that shares a listening port with an instance of Hapi.
+ 
+### Running the file
+
+```bash
+$ git clone git://github.com/dthree/vantage.git vantage
+$ cd ./vantage
+$ npm install
+$ node ./examples/hapi/hapi.js
+```
---
+++ examples/hapi/hapi.js
@@ -0,0 +1,45 @@
+
+/**
+ * Example of a Vantage server that hooks into
+ * a Hapi server.
+ *
+ * To run, ensure you have devDependencies 
+ * with `npm install`.
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var Vantage = require('./../../lib/vantage')
+  , Hapi = require('hapi')
+  , server = new Hapi.Server()
+  ;
+
+/**
+ * Variable declarations.
+ */
+
+var vantage
+  , delimiter = 'hapisvr:5000~$'
+  , port = 5000
+  ;
+
+/**
+ * Firing up a Vantage server.
+ */
+
+vantage = Vantage()
+ .delimiter(delimiter)
+ .listen(server, port)
+ .show();
+
+server.route({
+	method: 'GET',
+	path: '/hello-world',
+	handler: function(req, reply) {
+		reply('Hello world.');
+	}
+});
+
+server.start();
